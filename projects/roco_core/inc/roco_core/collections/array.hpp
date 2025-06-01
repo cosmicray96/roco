7b96e73dd99ec2c0c95a3e71dc519561a05f020a
cosmicray96 <cosmicrayleigh02@gmail.com>
2025-06-01 02:05:36 +0000

    working on array_iterator, needs testing

--- projects/roco_core/inc/roco_core/collections/array.hpp
+++ projects/roco_core/inc/roco_core/collections/array.hpp
@@ -15,113 +15,129 @@ namespace core {
 namespace collection {
 
 template <typename t_elem>
-    requires is_collection_elem<t_elem>
+  requires is_collection_elem<t_elem>
 class array {
-  public:
-    array(size_t capacity, allocators::allocator *allocator)
-        : m_data((t_elem *)(allocator->alloc(capacity * sizeof(t_elem)))), m_capacity(capacity),
-          m_count(0) {}
+public:
+  array(size_t capacity, allocators::allocator *allocator)
+      : m_data((t_elem *)(allocator->alloc(capacity * sizeof(t_elem)))),
+        m_capacity(capacity), m_count(0) {}
 
-    ~array() { allocators::registry::dealloc(m_data); }
+  ~array() { allocators::registry::dealloc(m_data); }
 
-    array(array &&other) noexcept
-        : m_data(other.m_data), m_capacity(other.m_capacity), m_count(other.m_count) {
-        other.m_data = nullptr;
-        other.m_capacity = 0;
-        other.m_count = 0;
+  array(array &&other) noexcept
+      : m_data(other.m_data), m_capacity(other.m_capacity),
+        m_count(other.m_count) {
+    other.m_data = nullptr;
+    other.m_capacity = 0;
+    other.m_count = 0;
+  }
+
+  array &operator=(array &&other) noexcept {
+    array new_array(std::move(other));
+    swap(new_array);
+    return *this;
+  }
+
+public:
+  static array<t_elem> copy(const array<t_elem> &other,
+                            allocators::allocator *a) {
+    array<t_elem> new_array(other.m_capacity, a);
+    new_array.m_count = other.m_count;
+    memcpy(new_array.m_data, other.m_data,
+           new_array.m_capacity * sizeof(t_elem));
+    return new_array;
+  }
+
+public:
+  t_elem &operator[](size_t index) {
+
+    if (index + 1 > m_count) {
+      m_count = index + 1;
     }
+    return m_data[index];
+  }
+  const t_elem &operator[](size_t index) const { return m_data[index]; }
 
-    array &operator=(array &&other) noexcept {
-        array new_array(std::move(other));
-        swap(new_array);
-        return *this;
+  friend std::ostream &operator<<(std::ostream &os, const array<t_elem> &arr)
+    requires roco_core::is_printable<t_elem>
+  {
+    if (arr.m_count == 0) {
+      os << "[ ]";
+      return os;
     }
-
-  public:
-    static array<t_elem> copy(const array<t_elem> &other, allocators::allocator *a) {
-        array<t_elem> new_array(other.m_capacity, a);
-        new_array.m_count = other.m_count;
-        memcpy(new_array.m_data, other.m_data, new_array.m_capacity * sizeof(t_elem));
-        return new_array;
+    if (arr.m_count == 1) {
+      os << "[ " << arr[0] << " ]";
+      return os;
     }
-
-  public:
-    t_elem &operator[](size_t index) {
-
-        if (index + 1 > m_count) {
-            m_count = index + 1;
-        }
-        return m_data[index];
+    os << "[ ";
+    for (size_t i = 0; i < arr.m_count - 1; i++) {
+      os << arr[i] << ", ";
     }
-    const t_elem &operator[](size_t index) const { return m_data[index]; }
+    os << arr[arr.m_count - 1] << " ]";
+    return os;
+  }
 
-    friend std::ostream &operator<<(std::ostream &os, const array<t_elem> &arr)
-        requires roco_core::is_printable<t_elem>
-    {
-        if (arr.m_count == 0) {
-            os << "[ ]";
-            return os;
-        }
-        if (arr.m_count == 1) {
-            os << "[ " << arr[0] << " ]";
-            return os;
-        }
-        os << "[ ";
-        for (size_t i = 0; i < arr.m_count - 1; i++) {
-            os << arr[i] << ", ";
-        }
-        os << arr[arr.m_count - 1] << " ]";
-        return os;
-    }
+public:
+  size_t count() const noexcept { return m_count; }
 
-  public:
-    size_t count() const noexcept { return m_count; }
+private:
+  void swap(array &other) noexcept {
+    std::swap(m_data, other.m_data);
+    std::swap(m_capacity, other.m_capacity);
+    std::swap(m_count, other.m_count);
+  }
 
-  private:
-    void swap(array &other) noexcept {
-        std::swap(m_data, other.m_data);
-        std::swap(m_capacity, other.m_capacity);
-        std::swap(m_count, other.m_count);
-    }
-
-  private:
-    t_elem *m_data;
-    size_t m_capacity;
-    size_t m_count;
+private:
+  t_elem *m_data;
+  size_t m_capacity;
+  size_t m_count;
 };
 
 template <typename t_elem> class array_iterator {
+public:
+  class dyn : public iterator<t_elem> {
   public:
-    class dyn : public iterator<t_elem> {
-      public:
-        dyn(array_iterator *arr_it) : self(&arr_it) {}
-        virtual ~dyn() = default;
-
-      public:
-        virtual void inc() override {}
-        virtual bool equals(const iterator<t_elem> &other) override {}
-        virtual t_elem get() override {}
-
-      private:
-        std::unique_ptr<array_iterator> self;
-    };
+    dyn(array_iterator *arr_it) : m_(&arr_it) {}
+    virtual ~dyn() = default;
+    dyn(const dyn &other) = default;
+    dyn &operator=(const dyn &other) = default;
+    dyn &operator=(dyn &&other) = default;
+    dyn(dyn &&other) = default;
 
   public:
-    array_iterator(array<t_elem>) {}
-    ~array_iterator() = default;
-    array_iterator(const array_iterator &other) {}
-    array_iterator &operator=(const array_iterator &other) {}
-    array_iterator &operator=(array_iterator &&other) {}
-    array_iterator(array_iterator &&other) {}
-
-  public:
-    iterator<t_elem> to_iterator() { return dyn{}; }
+    virtual void inc() override { m_->inc(); }
+    virtual bool equals(const iterator<t_elem> &other) override {
+      return m_->equals(dynamic_cast<dyn>(other).m_);
+    }
+    virtual t_elem get() override { return m_->get(); }
 
   private:
-    void swap(array_iterator &other) {}
+    std::unique_ptr<array_iterator> m_;
+  };
 
-  private:
-    t_elem *ptr;
+public:
+  array_iterator(t_elem *ptr) : m_ptr(ptr) {}
+  ~array_iterator() = default;
+  array_iterator(const array_iterator &other) = default;
+  array_iterator &operator=(const array_iterator &other) = default;
+  array_iterator &operator=(array_iterator &&other) = default;
+  array_iterator(array_iterator &&other) = default;
+
+public:
+  void inc() { m_ptr++; }
+  bool equals(const array_iterator<t_elem> &other) {
+    return m_ptr == other.m_ptr;
+  }
+  t_elem get() { return *m_ptr; }
+
+public:
+  iterator<t_elem> to_iterator() { return dyn{}; }
+
+private:
+  void swap(array_iterator &other) {}
+
+private:
+  t_elem *m_ptr;
 };
 
 } // namespace collection
