11f6a0efae34119bccc5a0496303bc6ac7c17cd5
cosmicray96 <cosmicrayleigh02@gmail.com>
2025-06-02 08:52:56 +0000

    minfix

--- projects/roco_core/inc/roco_core/allocators/allocator.hpp
+++ projects/roco_core/inc/roco_core/allocators/allocator.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <concepts>
 #include <cstddef>
 
 namespace roco {
@@ -8,20 +7,20 @@ namespace core {
 namespace allocators {
 
 class allocator {
-  public:
-    virtual void *alloc(size_t bytes) = 0;
-    virtual void dealloc(void *ptr) = 0;
-    virtual bool belongs(void *ptr) = 0;
+public:
+  virtual void *alloc(size_t bytes) = 0;
+  virtual void dealloc(void *ptr) = 0;
+  virtual bool belongs(void *ptr) = 0;
 };
 
 class registry {
-  private:
-    registry();
+private:
+  registry();
 
-  public:
-    static void init();
-    static void add(allocator *a);
-    static void dealloc(void *ptr);
+public:
+  static void init();
+  static void add(allocator *a);
+  static void dealloc(void *ptr);
 };
 
 } // namespace allocators
--- projects/roco_core/inc/roco_core/collections/collection.hpp
+++ projects/roco_core/inc/roco_core/collections/collection.hpp
@@ -11,17 +11,22 @@ namespace core {
 namespace collection {
 
 template <typename T>
-concept is_collection_elem = std::copyable<T> && std::movable<T> && std::default_initializable<T>;
+concept is_collection_elem =
+    std::copyable<T> && std::movable<T> && std::default_initializable<T>;
 
 template <typename t_elem> class collection {
-  public:
-    collection() = default;
-    virtual ~collection() = default;
+public:
+  collection() = default;
+  virtual ~collection() = default;
+  collection(const collection &other) = default;
+  collection &operator=(const collection &other) = default;
+  collection &operator=(collection &&other) = default;
+  collection(collection &&other) = default;
 
-  public:
-    virtual size_t count() = 0;
-    virtual iterator<t_elem> beg() = 0;
-    virtual iterator<t_elem> end() = 0;
+public:
+  virtual size_t count() = 0;
+  virtual iterator<t_elem> beg() = 0;
+  virtual iterator<t_elem> end() = 0;
 };
 
 } // namespace collection
