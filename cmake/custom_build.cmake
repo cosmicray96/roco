2c57feaf405bd6b5a0940e9064a3182a0e9fdcc6
cosmicray96 <cosmicrayleigh02@gmail.com>
2025-06-21 13:09:01 +0000

    fix

--- cmake/custom_build.cmake
+++ cmake/custom_build.cmake
@@ -1,8 +1,28 @@
 if(DEFINED passed_projects)
-	foreach(proj IN LIST ${passed_projects})
+	foreach(proj IN LISTS ${passed_projects})
 		add_subdirectory(${CMAKE_SOURCE_DIR}/projects/${proj})
 	endforeach()
 else()
-    message(WARNING "No projects passed")
+	message(WARNING "No projects passed")
+endif()
+
+
+if(DEFINED passed_tests)
+
+	file(GLOB subdirs LIST_DIRECTORIES true CONFIGURE_DEPENDS "${CMAKE_SOURCE_DIR}/projects/*")
+
+	set(project_dirs "")
+	foreach(entry IN LISTS subdirs)
+		if(IS_DIRECTORY "${entry}")
+			list(APPEND project_dirs "${entry}")
+		endif()
+	endforeach()
+
+	foreach(entry IN LISTS project_dirs)
+		if(EXISTS "${entry}/tests/CMakeLists.txt")
+			add_subdirectory("${entry}/tests/")
+		endif()
+	endforeach()
+
 endif()
 
