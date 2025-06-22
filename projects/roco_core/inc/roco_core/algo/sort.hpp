e5b6b737e9d8e349f22613405d8cbcda7927d95d
cosmicray96 <cosmicrayleigh02@gmail.com>
2025-06-22 09:09:21 +0000

    algo

--- projects/roco_core/inc/roco_core/algo/sort.hpp
+++ projects/roco_core/inc/roco_core/algo/sort.hpp
@@ -11,15 +11,25 @@ concept is_sortable = requires(T t) {
   { t == t } -> std::convertible_to<bool>;
   { t > t } -> std::convertible_to<bool>;
   { t < t } -> std::convertible_to<bool>;
+  {
+    swap(t, t)
+  } -> std::convertible_to<void>;
 };
-/*
+
 template <typename T>
   requires collections::is_iterator<T> &&
            is_sortable<typename T::t_elem>
-T::t_coll sort(T it1, T it2) {
+void sort(T it1, T it2) {
 
+  while (it1 != it2) {
+
+    if (*it1 > *it2) {
+      swap(*it1, *it2);
+    }
+
+    it1.inc();
+  }
 }
-*/
 
 } // namespace algo
 
